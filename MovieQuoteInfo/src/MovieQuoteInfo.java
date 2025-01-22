96a943bb9c8c6b035250df0dbd033d536e103a50
Andrew-Kirk <ragusauces21@gmail.com>
2025-01-22 17:56:35 -0500

    Add files via upload

---
+++ MovieQuoteInfo/src/MovieQuoteInfo.java
@@ -0,0 +1,12 @@
+// Andrew Kirk 1/16/25 Movie Quote Assignment
+
+public class MovieQuoteInfo {
+	
+		public static void main(String[] args) {
+			
+			System.out.println("\"It's Dr. Evil, I didn't spend six years in Evil Medical School to be called \"mister,\" thank you very much.\"");
+			System.out.println("Dr. Evil, in Austin Powers: International Man Of Mystery (1997)");
+
+	}
+
+}
